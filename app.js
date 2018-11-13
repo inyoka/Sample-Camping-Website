b30dcc37695aa2bd0d8085661e9ca5eaf83d61ef
Simon <simonmarkholland@gmail.com>
2018-11-13 10:43:13 +0700

    Initial app.js with landing message.

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+  res.send('Soon to be landing page.');
+});
+
+
+// This differs from the course logging the port and IP being used.
+// ... I am running locally not on c9.
+// Express 4.x app.listen() is an asynchronous operation, hence the 'var listener'.
+var listener = app.listen(8000, '127.0.0.1', function(){
+  var address = listener.address().address;
+  var port = listener.address().port;
+
+  console.log('YelpCamp server listening on : ' + address + ':' + port);
+});
