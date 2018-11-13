77a7c9812e7456912244ff3b6ec34a047e46be4d
Simon <simonmarkholland@gmail.com>
2018-11-13 19:57:46 +0700

    Allow user to add temporary campsite.

--- app.js
+++ app.js
@@ -5,22 +5,28 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 
+var campgrounds = [
+    {name: 'Salmons Creek', image: 'https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104491f2c17daee9b4b9_340.jpg'},
+    {name: 'Granite Hill', image: 'https://pixabay.com/get/e83db50a2ff5083ed1584d05fb1d4e97e07ee3d21cac104491f2c17daee9b4b9_340.jpg'},
+    {name: "Mountain Goat's Rest", image: 'https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104491f2c17daee9b4b9_340.jpg'}
+  ];
+
 app.get('/', function(req, res){
   res.render('landing');
 });
 
 // Images from https://www.photosforclass.com/search?text=camping
 app.get('/campgrounds', function(req, res){
-    var campgrounds = [
-      {name: 'Salmons Creek', image: 'https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104491f2c17daee9b4b9_340.jpg'},
-      {name: 'Granite Hill', image: 'https://pixabay.com/get/e83db50a2ff5083ed1584d05fb1d4e97e07ee3d21cac104491f2c17daee9b4b9_340.jpg'},
-      {name: "Mountain Goat's Rest", image: 'https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104491f2c17daee9b4b9_340.jpg'}
-    ]
+
     res.render('campgrounds', {campgrounds: campgrounds});
 });
 
 app.post('/campgrounds', function(req, res){
-  res.send("Campground POST route.");
+  var name = req.body.name;
+  var image = req.body.image;
+  var newCampground = {name: name, image: image}
+  campgrounds.push(newCampground);
+  res.redirect('/campgrounds');
 });
 
 app.get('/campgrounds/new', function(req, res){
