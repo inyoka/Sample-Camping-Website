372758566d4abf45b103667373b837637b7870bf
Simon <simonmarkholland@gmail.com>
2018-11-13 11:02:21 +0700

    Add campgrounds route and page

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ app.get('/', function(req, res){
   res.render('landing');
 });
 
+// Images from https://www.photosforclass.com/search?text=camping
+app.get('/campgrounds', function(req, res){
+    var campgrounds = [
+      {name: 'Salmons Creek', image: 'https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104491f2c17daee9b4b9_340.jpg'},
+      {name: 'Granite Hill', image: 'https://pixabay.com/get/e83db50a2ff5083ed1584d05fb1d4e97e07ee3d21cac104491f2c17daee9b4b9_340.jpg'},
+      {name: "Mountain Goat's Rest", image: 'https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104491f2c17daee9b4b9_340.jpg'}
+    ]
+
+    res.render('campgrounds');
+});
 
 // This differs from the course logging the port and IP being used.
 // ... I am running locally not on c9.
