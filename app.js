02162cb287166825389fc3fd6d6f100059f3e8f1
Simon <simonmarkholland@gmail.com>
2018-11-26 08:13:53 +0700

    Alter campground to include description.

--- app.js
+++ app.js
@@ -12,10 +12,30 @@ app.set('view engine', 'ejs');
 
 var campgroundSchema = new mongoose.Schema({
   name: String,
-  image: String
+  image: String,
+  description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
+
+
+// Uncomment to add a campground ...
+
+// Campground.create(
+//   {
+//     name: 'Granite Hill',
+//     image: 'imgs/camp-02.jpg',
+//     description: 'This is a huge granite hill, no bathrooms, no water. Beautiful granite.'
+//   },
+//   function(err, campground){
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log("Newly created campground ...");
+//       console.log(campground);
+//     }
+//   }
+// );
 
 app.get('/', function(req, res){
   res.render('landing');
