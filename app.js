9f8bfd1afdffd1b08b1130b258dc084417756215
Simon <simonmarkholland@gmail.com>
2018-11-26 10:57:21 +0700

    Add description to model and template.

--- app.js
+++ app.js
@@ -60,7 +60,8 @@ app.get('/campgrounds', function(req, res){
 app.post('/campgrounds', function(req, res){
   var name = req.body.name;
   var image = req.body.image;
-  var newCampground = {name: name, image: image}
+  var desc = req.body.description;
+  var newCampground = {name: name, image: image, description: desc}
   //  Create new campground and save to DB.
   Campground.create(newCampground, function(err, newlyCreated){
     if(err){
