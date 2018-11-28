97a0600774bdcf3b3c398a48197de9595bd54bce
Simon <simonmarkholland@gmail.com>
2018-11-28 14:21:43 +0700

    Mv campground schema to seperate file.

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var mongoose = require("mongoose");
-
+var express = require('express'),
+app = express(),
+bodyParser = require('body-parser'),
+mongoose = require("mongoose"),
+Campground = require("./models/campground"),
 mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser : true });
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname + '/public'));
