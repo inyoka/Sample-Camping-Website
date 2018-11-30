a381d38b612e047d677190ed4616d4b358baafa9
Simon <simonmarkholland@gmail.com>
2018-11-30 11:07:34 +0700

    Reformat imports for readability.

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var express = require('express'),
-app = express(),
-bodyParser = require('body-parser'),
-mongoose = require("mongoose"),
-Campground = require("./models/campground"),
-Comment = require("./models/comment"),
-seedDB = require("./seeds");
+    app = express(),
+    bodyParser = require('body-parser'),
+    mongoose = require("mongoose"),
+    Campground = require("./models/campground"),
+    Comment = require("./models/comment"),
+    seedDB = require("./seeds");
 
 mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser : true });
 app.use(bodyParser.urlencoded({extended:true}));
