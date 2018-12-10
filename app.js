7ceb0843b5e9b7b1b927dc93911535da67571ce8
Simon <simonmarkholland@gmail.com>
2018-12-10 17:19:04 +0000

    Pass user info to templates, alter header logged in status accordingly.

--- app.js
+++ app.js
@@ -29,6 +29,11 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.use(function(req, res, next){
+  res.locals.currentUser = req.user;
+  next();
+});
+
 // Schema Setup
 
 app.get('/', function(req, res){
