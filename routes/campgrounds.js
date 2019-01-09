9f18c4324a8f61a340989f30bdf9c28a683e86a7
Simon <simonmarkholland@gmail.com>
2019-01-09 12:26:37 +0000

    Add delete button and method.

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -80,6 +80,15 @@ router.put("/:id", function(req, res){
   });
 });
 
+// DESTROY CAMPGROUND ROUTE 
+router.delete("/:id", function(req, res){
+  Campground.findByIdAndRemove(req.params.id, function(err){
+    if (err) {
+      res.redirect("/campgrounds");
+    } else {
+      res.redirect("/campgrounds");
+    }
+  });
 function isLoggedIn(req, res, next){
   if(req.isAuthenticated()){
     return next();
