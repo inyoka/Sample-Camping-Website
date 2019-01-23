5dcaf8dd587120890c1de4679c945eb1e8ea89b6
Simon <simonmarkholland@gmail.com>
2019-01-23 07:53:38 +0700

    Fix bug preventing error message without double click.

--- routes/index.js
+++ routes/index.js
@@ -22,7 +22,7 @@ router.post('/register', function(req, res){
   User.register(newUser, req.body.password, function(err, user){
       if(err){
         console.log(err);
-        return res.render("register");
+        return res.redirect("/register");
       }
       passport.authenticate("local")(req, res, function(){
         res.redirect("/campgrounds");
