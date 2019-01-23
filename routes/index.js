639cbaebd5cac6848a0f2c353ceb5d9f5dfda89d
Simon <simonmarkholland@gmail.com>
2019-01-23 07:54:03 +0700

    Remove unused console log.

--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,6 @@ router.post('/register', function(req, res){
   var newUser = new User({username: req.body.username});
   User.register(newUser, req.body.password, function(err, user){
       if(err){
-        console.log(err);
         return res.redirect("/register");
       }
       passport.authenticate("local")(req, res, function(){
