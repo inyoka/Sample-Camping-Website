0f82bf7062d0f22bfcb2a7fab5b28c134cf91d4a
Simon <simonmarkholland@gmail.com>
2019-01-23 07:55:36 +0700

    Add Flash messages to middleware

--- middleware/index.js
+++ middleware/index.js
@@ -7,16 +7,19 @@ middlewareObj.checkCampgroundOwnership = function(req, res, next){
     if(req.isAuthenticated()){
         Campground.findById(req.params.id, function(err, foundCampground){
         if (err) {
+            req.flash("error", "Campground was not found.");
             res.redirect("back");
         } else {
             if(foundCampground.author.id.equals(req.user._id)){
                 next();
             } else {
+                req.flash("error", "Please ask Campground owner to do this.");
                 res.redirect("back");
             }
         }
         });
     } else {
+        req.flash("error", "Please login first.");
         res.redirect("back");
     }
 }
@@ -25,16 +28,18 @@ middlewareObj.checkCommentOwnership = function checkCommentOwnership(req, res, n
     if(req.isAuthenticated()){
         Comment.findById(req.params.comment_id, function(err, foundComment){
         if (err) {
-          res.redirect("back");
-        } else {
-          if(foundComment.author.id.equals(req.user._id)){
-            next();
-          } else {
             res.redirect("back");
-          }
+        } else {
+            if(foundComment.author.id.equals(req.user._id)){
+                next();
+            } else {
+                req.flash("error", "Only the comment owner can do that.");
+                res.redirect("back");
+            }
         }
         });
     } else {
+        req.flash("error", "Please login first.");
         res.redirect("back");
     }
 };
@@ -43,6 +48,7 @@ middlewareObj.isLoggedIn = function(req, res, next){
     if(req.isAuthenticated()){
         return next();
     }
+    req.flash("error", "Please login first!!");
     res.redirect("/login");
 }
 
